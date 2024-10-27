a742f3e66b54fac3f5c77e4bf4b780bd3d6926d5
Андрей Рубан <anruban2001@gmail.com>
2024-10-27 21:47:28 +0200

    add pr3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,34 @@
-import eslist from "@eslint/js";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
 
 export default tseslint.config(
-    eslist.configs.recommended,
-    ... tseslint.configs.recommended
+  { ignores: ['dist'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended
+    ],
+    files: ['**/*.{ts}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        project: ['tsconfig.json',],
+      },
+    },
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-console': 'error',
+      'prefer-const': 'warn',
+      '@typescript-eslint/no-unused-vars': 'off'
+    },
+  }
 );
